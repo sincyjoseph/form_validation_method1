2345caf9300d0a23766a9083e16bf8382969c626
SINCY JOSEPH <sincyjoseph535@gmail.com>
2021-09-02 18:57:25 +0530

    minor change

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="main.css">
-    <style>
-
-    </style>
 </head>
 <br/>
 <body>
