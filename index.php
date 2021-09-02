493b06f6e5dd6a57731f23dcc534349c60b92ed1
SINCY JOSEPH <sincyjoseph535@gmail.com>
2021-09-02 18:40:01 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <style>
+
+    </style>
+</head>
+<br/>
+<body>
+    
+<form id="first_form" method="post" action="">
+  <div>
+    <label for="first_name">First Name:</label>
+    <input type="text" id="first_name" name="first_name"></input>
+  </div>
+  <div>
+    <label for="last_name">Last Name:</label>
+    <input type="text" id="last_name" name="last_name"></input>
+  </div>
+  <div>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email"></input>
+  </div>
+  <div>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password"></input>
+  </div>
+
+  <div>
+    <input type="submit" value="Submit" />
+  </div>
+</form>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="register.js"></script>
+</body>
+</html>
---
+++ register.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    $('#first_form').submit(function(e) {
+      var first_name = $('#first_name').val();
+      var last_name = $('#last_name').val();
+      var email = $('#email').val();
+      var password = $('#password').val();
+  
+      $(".error").remove();
+  
+      if (first_name.length < 1) {
+        $('#first_name').after('<span class="error">This field is required</span>');
+        e.preventDefault();
+      }
+      if (last_name.length < 1) {
+        $('#last_name').after('<span class="error">This field is required</span>');
+        e.preventDefault();
+      }
+      if (email.length < 1) {
+        $('#email').after('<span class="error">This field is required</span>');
+        e.preventDefault();
+      }
+      if (password.length < 8) {
+        $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
+        e.preventDefault();
+      }
+    });
+  
+  });
